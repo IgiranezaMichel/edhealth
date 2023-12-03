29bf3ce9c961cb467e3b7728172a650f9831b407
IgiranezaMichel <igiramichael@gmail.com>
2023-12-02 23:08:24 -0800

    add data:{institutuionName:string} props in AcustomNavBar

--- src/Component/StudentComponents/AcustomNavBar.tsx
+++ src/Component/StudentComponents/AcustomNavBar.tsx
@@ -1,4 +1,4 @@
-const AcustomNavBar=()=>{
+const AcustomNavBar=(data:{institutuionName:string})=>{
     return(
     <main className="bg-white fw-bolder display-5 w-100 border-bottom p-3" style={{fontFamily:'fantasy',float:'right'}}>
        <span><i className="bi bi-droplet" ></i><span className="text-dark">ED</span><span className="text-info">HEALTH</span></span>
