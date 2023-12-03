dc81ff63b206e323d9abfbf17d7de18470d35423
IgiranezaMichel <igiramichael@gmail.com>
2023-12-03 00:51:47 -0800

    renaming props variable of institutuion to institution

--- src/Component/StudentComponents/AcustomNavBar.tsx
+++ src/Component/StudentComponents/AcustomNavBar.tsx
@@ -1,8 +1,8 @@
-const AcustomNavBar=(data:{institutuionName:string})=>{
+const AcustomNavBar=(data:{institutionName:string})=>{
     return(
     <main className="bg-white fw-bolder display-5 w-100 border-bottom p-3" style={{fontFamily:'fantasy',float:'right'}}>
        <span><i className="bi bi-droplet" ></i><span className="text-dark">ED</span><span className="text-info">HEALTH</span></span>
-        <span style={{float:'right'}}> <i className="bi bi-person" ></i><span className="text-dark">{data.institutuionName}</span></span>
+        <span style={{float:'right'}}> <i className="bi bi-person" ></i><span className="text-dark">{data.institutionName}</span></span>
     </main>
     )
 }
--- src/Pages/Hospital/HospitalHome.tsx
+++ src/Pages/Hospital/HospitalHome.tsx
@@ -3,14 +3,19 @@ import SideBar from "../../Component/StudentComponents/SideBar";
 
 const HospitalHome=()=>{
 return(
-<main className="row col-12 m-auto">
-<AcustomNavBar institutuionName="Hospital Name"/>
-   <section className="col-md-3">
+<main >
+
+<div className="sticky-top">
+<AcustomNavBar institutionName="Hospital Name"/>
+</div>
+<section className="row col-12 m-auto  mt-5 overflow-auto">
+   <section className="col-md-3 overflow-auto" >
         <SideBar/>
     </section> 
-    <section className="col-md-9">
-        <div className="card">
-            <div className="col-md-10 m-auto g-3 row">
+    <section className="col-md-9 overflow-auto">
+        <div className="card px-3 py-3 mt-3">
+            <span className="fw-bolder fs-4 text-uppercase">Publish Training</span>
+            <div className="col-md-12 m-auto g-3 row">
                 <div className="form-floating mb-3 col-md-4">
                     <input type="text" className="form-control" name="formId1" id="formId1" placeholder=""/>
                     <label className="fw-bolder">Title</label>
@@ -25,9 +30,59 @@ return(
                     <label className="fw-bolder">Deadline</label>
                 </div>
             </div>
-            <button className="text-uppercase btn" style={{backgroundColor:'grey'}}>Register</button>
+            <button className="text-uppercase btn w-25 d-block align-self-center text-white fw-bolder" style={{backgroundColor:'#49579F'}}>Register</button>
+            <div>
+            <span className="" style={{float:'right',color:'#49579F'}} >View Trainings</span>
+            </div>
         </div>
-    </section>     
+        {/*  */}
+        <div className="card px-3 py-3 mt-3">
+            <span className="fw-bolder fs-4 text-uppercase">Publish Jobs</span>
+            <div className="col-md-12 m-auto g-3 row">
+                <div className="form-floating mb-3 col-md-4">
+                    <input type="text" className="form-control" name="formId1" id="formId1" placeholder=""/>
+                    <label className="fw-bolder">Title</label>
+                </div>
+                <div className="form-floating mb-3 col-md-4">
+                   <div className="mb-3">
+                     <textarea className="form-control" name="" placeholder="Discription ....." id="" rows={2}></textarea>
+                   </div>
+                </div>
+                <div className="form-floating mb-3 col-md-4">
+                    <input type="date" className="form-control" name="formId1" id="formId1" placeholder=""/>
+                    <label className="fw-bolder">Deadline</label>
+                </div>
+            </div>
+            <button className="text-uppercase btn w-25 d-block align-self-center text-white fw-bolder" style={{backgroundColor:'#49579F'}}>Register</button>
+            <div>
+            <span className="" style={{float:'right',color:'#49579F'}} >View Trainings</span>
+            </div>
+        </div>
+        {/*  */}
+        <div className="card px-3 py-3 mt-3">
+            <span className="fw-bolder fs-4 text-uppercase">Add Certificate</span>
+            <div className="col-md-12 m-auto g-3 row">
+                <div className="form-floating mb-3 col-md-4">
+                    <input type="text" className="form-control" name="formId1" id="formId1" placeholder=""/>
+                    <label className="fw-bolder">Title</label>
+                </div>
+                <div className="form-floating mb-3 col-md-4">
+                   <div className="mb-3">
+                     <textarea className="form-control" name="" placeholder="Discription ....." id="" rows={2}></textarea>
+                   </div>
+                </div>
+                <div className="form-floating mb-3 col-md-4">
+                    <input type="date" className="form-control" name="formId1" id="formId1" placeholder=""/>
+                    <label className="fw-bolder">Deadline</label>
+                </div>
+            </div>
+            <button className="text-uppercase btn w-25 d-block align-self-center text-white fw-bolder" style={{backgroundColor:'#49579F'}}>Register</button>
+            <div>
+            <span className="" style={{float:'right',color:'#49579F'}} >View Trainings</span>
+            </div>
+        </div>
+    </section>  
+</section>   
 </main>
 )}
 export default HospitalHome;
