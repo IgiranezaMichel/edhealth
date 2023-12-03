b1fc0adf50bb2d1838a2e62248b7e3ce4d972222
IgiranezaMichel <igiramichael@gmail.com>
2023-12-02 23:16:57 -0800

    Adding User inputs of publishing trainings

--- src/Pages/Hospital/HospitalHome.tsx
+++ src/Pages/Hospital/HospitalHome.tsx
@@ -4,17 +4,27 @@ import SideBar from "../../Component/StudentComponents/SideBar";
 const HospitalHome=()=>{
 return(
 <main className="row col-12 m-auto">
-<AcustomNavBar/>
+<AcustomNavBar institutuionName="Hospital Name"/>
    <section className="col-md-3">
-    <SideBar/>
-    </section>
-    <div className="form-floating mb-3">
-      <input
-        type="text"
-        className="form-control" name="formId1" id="formId1" placeholder=""/>
-      <label>Name</label>
-    </div>
-    <section>
+        <SideBar/>
+    </section> 
+    <section className="col-md-9">
+        <div className="card">
+            <div className="col-md-10 m-auto g-3 row">
+                <div className="form-floating mb-3 col-md-4">
+                    <input type="text" className="form-control" name="formId1" id="formId1" placeholder=""/>
+                    <label>Name</label>
+                </div>
+                <div className="form-floating mb-3 col-md-4">
+                    <input type="text" className="form-control" name="formId1" id="formId1" placeholder=""/>
+                    <label>Name</label>
+                </div>
+                <div className="form-floating mb-3 col-md-4">
+                    <input type="text" className="form-control" name="formId1" id="formId1" placeholder=""/>
+                    <label>Name</label>
+                </div>
+            </div>
+        </div>
     </section>     
 </main>
 )}
