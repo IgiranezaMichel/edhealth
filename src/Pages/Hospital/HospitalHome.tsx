5f68866eceb593e0073d0c29c6e79646f87d6d0f
IgiranezaMichel <igiramichael@gmail.com>
2023-12-02 23:23:45 -0800

    Styling Publish trainings

--- src/Pages/Hospital/HospitalHome.tsx
+++ src/Pages/Hospital/HospitalHome.tsx
@@ -13,17 +13,19 @@ return(
             <div className="col-md-10 m-auto g-3 row">
                 <div className="form-floating mb-3 col-md-4">
                     <input type="text" className="form-control" name="formId1" id="formId1" placeholder=""/>
-                    <label>Name</label>
+                    <label className="fw-bolder">Title</label>
                 </div>
                 <div className="form-floating mb-3 col-md-4">
-                    <input type="text" className="form-control" name="formId1" id="formId1" placeholder=""/>
-                    <label>Name</label>
+                   <div className="mb-3">
+                     <textarea className="form-control" name="" placeholder="Discription ....." id="" rows={2}></textarea>
+                   </div>
                 </div>
                 <div className="form-floating mb-3 col-md-4">
-                    <input type="text" className="form-control" name="formId1" id="formId1" placeholder=""/>
-                    <label>Name</label>
+                    <input type="date" className="form-control" name="formId1" id="formId1" placeholder=""/>
+                    <label className="fw-bolder">Deadline</label>
                 </div>
             </div>
+            <button className="text-uppercase btn" style={{backgroundColor:'grey'}}>Register</button>
         </div>
     </section>     
 </main>
