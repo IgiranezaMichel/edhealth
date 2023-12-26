4e3ba04aae354de624d75a08668f2d1aca3e27d8
IgiranezaMichel <igiramichael@gmail.com>
2023-12-26 21:24:34 +0200

    add Sidebar

--- src/Pages/ncnm/NcnmHome.tsx
+++ src/Pages/ncnm/NcnmHome.tsx
@@ -1,13 +1,16 @@
 import AcustomNavBar from "../../Component/StudentComponents/AcustomNavBar";
 import SideBar from "../../Component/StudentComponents/SideBar";
-
+import {ToastContainer, toast} from 'react-toastify'
 const NcnmHome=()=>{
+const click=()=>{
+    toast('Clicked')
+}
 return(
 <>
 <main className="row m-auto col-12">
     <AcustomNavBar institutionName="NCNM"/>
     <section className="sticky-top top-50 col-md-3">
-        <SideBar class="text-primary fw-bolder"/>
+        <SideBar class="text-secondary fw-bolder"/>
     </section>
     <section className="col-md-7 m-auto">
         <div className="table-responsive">
@@ -18,7 +21,8 @@ return(
                     <td colSpan={2}>
                         <div>
                           <input type="text"
-                              className="w-75 rounded-4" style={{boxShadow:'2px 4 px blue'}} name="" id="" aria-describedby="helpId" placeholder="Search ..."/><button type="button" className="btn rounded-circle bg-dark text-white m-2 py-2"><i className="bi bi-search" aria-hidden="true"></i></button>
+                              className="w-75 rounded-4" style={{boxShadow:'2px 4 px blue'}} name="" id="" aria-describedby="helpId" placeholder="Search ..."/>
+                              <button onClick={click} type="button" className="btn rounded-circle bg-dark text-white m-2 py-2"><i className="bi bi-search" aria-hidden="true"></i></button>
                         </div>
                     </td>
                 </tr>
@@ -43,6 +47,7 @@ return(
         </div>
     </section>
 </main>
+<ToastContainer/>
 </>
 )
 }
