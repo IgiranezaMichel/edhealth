d4cf57c20d6e64cce49c5b4272b29e1822e10f89
IgiranezaMichel <igiramichael@gmail.com>
2023-12-26 21:24:45 +0200

    add signup

--- src/Pages/Visitor/Auth/Signup.tsx
+++ src/Pages/Visitor/Auth/Signup.tsx
@@ -1,62 +1,56 @@
+import { ToastContainer ,toast} from "react-toastify";
 import Navigation from "../../../Component/Visitor/Navigation";
-
+import {useState,useEffect} from 'react'
+import 'react-toastify/dist/ReactToastify.css';
 const Signup=()=>{
+const [username,setUserName]=useState('');
+const [password,setPassword]=useState('');
+useEffect(()=>{
+console.log('username '+username+'password =>'+password)
+})
+
+const validateForm=()=>{
+  username==''?toast.error('Provide email')
+  :
+  password==''?toast('Provide password'):
+  toast.success('Done!') ;
+}
     return(
         <>
         <main>
         <Navigation/>
-        <section className="row m-auto col-md-9 py-5">
-            <div className="card col-md-7 rounded-0 p-0 border-0 d-sm-none d-md-block d-lg-block" style={{display:'flex',justifyContent:'center'}}>
+        <section className="p-5" style={{backgroundImage:'url(/visit/bg.jpeg)',backgroundSize:'cover',backgroundPosition:'fixed'}}>
+        <section className="row container-lg m-auto mt-5">
+            <div className="card col-md-5 d-flex justify-content-center align-content-center rounded-0 p-0 border-0">
                <div>
-                <div className="card bg-dark text-white">
-                    <img className="card-img" src="doctor.png" alt="Title"/>
+                <div className="card p-0 rounded-0 border-0">
+                    <img className="card-img" src="/visit/doctor.jpeg" alt="Title"/>
                 </div>
                </div>
             </div>
-            <div className="card col-md-5 p-5 rounded-0 col-12">
-                <span className="d-block display-6 fw-bolder text-center py-4">Create account</span>
-                <div className="form-floating mb-3">
-                  <input
-                    type="text"
-                    className="form-control" name="email" id="formId1" placeholder=""/>
-                  <label className="fw-bolder">Name</label>
-                </div>
-                <div className="form-floating mb-3">
-                  <input
-                    type="text"
-                    className="form-control" name="formId1" id="formId1" placeholder=""/>
-                  <label className="fw-bolder">National Id</label>
-                </div>
-                <div className="form-floating mb-3">
-                  <input
-                    type="text"
-                    className="form-control" name="email" id="formId1" placeholder=""/>
-                  <label className="fw-bolder">Email</label>
-                </div>
-                <div className="form-floating mb-3">
-                  <input
-                    type="text"
-                    className="form-control" name="email" id="formId1" placeholder=""/>
-                  <label className="fw-bolder">Phone Number</label>
-                </div>
-                <div className="form-floating mb-3">
-                  <input
-                    type="text"
-                    className="form-control" name="email" id="formId1" placeholder=""/>
-                  <label className="fw-bolder">Email</label>
-                </div>
-                <div className="form-floating mb-3">
-                  <input
-                    type="password"
-                    className="form-control" name="formId1" id="formId1" placeholder=""/>
-                  <label className="fw-bolder">Password</label>
-                </div>
-                <span className="text-muted d-block">Do you have an account?</span>
-                <span className="text-primary text-uppercase">Login</span>
-                <button type="button" className="btn btn-outline-primary mt-2 d-block">Register</button>
-            </div>
+            <section className="card col-md-7 p-5 d-flex justify-content-center align-content-center rounded-0 col-12">
+             <section className="row g-3">
+              <span className="d-block text-center display-6 fw-bold">Login form</span>
+               <div>
+                 <label  className="form-label">Email</label>
+
+                 <input value={username} type="text" onChange={(t=>(setUserName(t.target.value)))} className="form-control"/>
+               </div>
+   
+               <div>
+                 <label  className="form-label">Password</label>
+                 <input value={password} type="password" onChange={(t=>(setPassword(t.target.value)))} className="form-control" />
+               </div>
+  
+               <div className="col-12 modal-footer">
+                 <button className="btn btn-primary px-3 fw-bold" onClick={validateForm}>Login</button>
+               </div>
+             </section>
+            </section>
             {/* <Link to={'signup'} className="text-bg-primary">Register</Link> */}
         </section>
+        </section>
+        <ToastContainer/>
     </main>
         </>
     )
