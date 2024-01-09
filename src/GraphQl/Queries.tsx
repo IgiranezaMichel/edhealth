ef216ab4d52573a14973814c0c2fb627289fdfba
IgiranezaMichel <igiramichael@gmail.com>
2024-01-09 19:52:04 +0200

    Add Queries Component

--- src/GraphQl/Queries.tsx
+++ src/GraphQl/Queries.tsx
@@ -9,4 +9,117 @@ query findTrainingById($id:Long){
         postDate
     }
 }
+`;
+export const GET_ALL_USER=gql`
+query{
+    getAllUsers{
+        name
+        email
+        phoneNumber
+        profilePicture
+        dob
+        gender
+    }
+}`;
+export const USER_lIST_PAGINATION=gql`
+query($pageNumber:Int,$pageSize:Int,$sortBy:String){
+    useListPagination(pageNumber:$pageNumber,pageSize:$pageSize,sortBy:$sortBy){
+        content{
+            id
+            name
+            profilePicture
+            email
+            phoneNumber
+            dob
+            gender
+            role
+        }
+        pageNumber
+        pageSize
+        size
+    }
+}
+`;
+// hospital
+export const HOSPITAL_lIST_PAGINATION=gql`
+query($pageNumber:Int,$pageSize:Int,$sortBy:String){
+    hospitaListPagination(pageNumber:$pageNumber,pageSize:$pageSize,sortBy:$sortBy){
+        content{
+            id
+            name
+            logo
+            createdOn
+            location{
+                name
+                location{
+                    name
+                    location{
+                        name
+                    }
+                }
+            }
+        }
+        pageNumber
+        pageSize
+        size
+    }
+}
+`;
+
+export const SCHOOL_lIST_PAGINATION=gql`
+query($pageNumber:Int,$pageSize:Int,$sortBy:String){
+    schoolListPagination(pageNumber:$pageNumber,pageSize:$pageSize,sortBy:$sortBy){
+        content{
+            id
+            name
+            logo
+            createdOn
+            location{
+                name
+                location{
+                    name 
+                    location{
+                    name  
+                } 
+                }
+            }
+        }
+        pageNumber
+        pageSize
+        size
+    }
+}
+`;
+export const SCHOOL_SIZE=gql`
+query{
+    schoolSize
+}
+`;
+// location
+export const GET_ALL_LOCATION=gql`
+query {
+    getAllLocations{
+        id
+        name
+        type
+       locationList{
+        id
+        name
+        type
+        locationList{
+         id
+        name
+        type
+        }
+      }
+    }
+}
+`;
+export const TRAINING_LIST_PAGINATION=gql`
+query($pageNumber:Int,$pageSize:Int,$sortBy:String,$behaviour:String,$status:Boolean){
+    trainingListPagination(pageNumber:$pageNumber,pageSize:$pageSize,sortBy:$sortBy,behaviour:$behaviour,status:$status){
+        title
+        description
+    } 
+}
 `
