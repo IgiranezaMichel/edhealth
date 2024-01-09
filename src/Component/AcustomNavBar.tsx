5e5aec8de44b38368a7b6f91b9a2ffc429f11025
IgiranezaMichel <igiramichael@gmail.com>
2024-01-09 19:47:09 +0200

    Add AcustomNavBar Component

---
+++ src/Component/AcustomNavBar.tsx
@@ -0,0 +1,9 @@
+const AcustomNavBar=(data:{institutionName:string})=>{
+    return(
+    <main className="bg-white fw-bolder w-100 border-bottom p-3" style={{float:'right'}}>
+       <span className="display-6 fw-bolder"><i className="bi bi-droplet" ></i><span className="text-dark">ED</span><span className="text-info">HEALTH</span></span>
+        <span className="fs-4" style={{float:'right'}}> <i className="bi bi-house-fill mx-2" ></i><span className="text-dark">{data.institutionName}</span></span>
+    </main>
+    )
+}
+export default AcustomNavBar;
