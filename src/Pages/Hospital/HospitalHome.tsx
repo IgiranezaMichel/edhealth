acb66bd7e0931dfd3491fb259531a44b6efdecba
IgiranezaMichel <igiramichael@gmail.com>
2024-01-09 19:54:57 +0200

    Add crud for HospitalHome

--- src/Pages/Hospital/HospitalHome.tsx
+++ src/Pages/Hospital/HospitalHome.tsx
@@ -1,16 +1,38 @@
 import { useMutation, useQuery } from "@apollo/client";
-import AcustomNavBar from "../../Component/StudentComponents/AcustomNavBar";
-import SideBar from "../../Component/StudentComponents/SideBar";
 import { useState, useEffect } from 'react'
 import { toast, ToastContainer } from 'react-toastify'
 import { SAVE_TRAINING } from "../../GraphQl/Mutations";
-import { FIND_TRAINING_BY_ID } from "../../GraphQl/Queries";
-const HospitalHome = () => {
+import { FIND_TRAINING_BY_ID, TRAINING_LIST_PAGINATION } from "../../GraphQl/Queries";
+import { HospitalNavigation } from "./HospitalNavigation";
+import { Link } from "react-router-dom";
+
+export const HospitalHome = () => {
+
+    return (
+        <HospitalNavigation >
+            {Training()}
+            {Job()}
+            {Certificate()}
+            <ToastContainer />
+        </HospitalNavigation>
+    )
+}
+function Training() {
     const [trainingTitle, setTrainingTitle] = useState('');
     const [trainingDescription, setTrainingDescription] = useState('');
+    const [trainer, setTrainer] = useState('');
     const [trainingDeadline, setTrainingDeadline] = useState('');
-    const [saveTraining, TrainingResult] = useMutation(SAVE_TRAINING);
-
+    const [pageNumber, setPageNumber] = useState(0);
+    const [pageSize, setPageSize] = useState(0);
+    const [sortBy, setSortBy] = useState('');
+    const [behaviour, setBehaviour] = useState('appending');
+    const [status, setStatus] = useState(false);
+    const [saveTraining] = useMutation(SAVE_TRAINING);
+    const {data}=useQuery(TRAINING_LIST_PAGINATION,{
+        variables:{
+            pageNumber:pageNumber,pageSize:pageSize,sortBy:sortBy,behaviour:behaviour,status:status
+        }
+    })
     const findTrainingByIdResult = useQuery(FIND_TRAINING_BY_ID, {
         variables: {
             id: 2
@@ -18,118 +40,111 @@ const HospitalHome = () => {
     });
     useEffect(() => {
         findTrainingByIdResult
+        if(data)
+        console.log(data)
     }, [2])
     console.log(findTrainingByIdResult.data)
-    const submitTrainingForm = () => {
-        const inputTraining: inputTraining = {
+    const submitTrainingForm = async() => {
+        const inputTraining={
             id: 0,
-            title: trainingTitle,
-            deadline: trainingDeadline,
-            description: trainingDescription,
-            name: ''
+            title:trainingTitle,
+            description:trainingDescription,
+            deadline:trainingDeadline,
+            trainers:trainer,
+            hospitalId:1
         }
-        saveTraining({
+      await  saveTraining({
             variables: {
-                input: inputTraining
+                input:inputTraining
             }
-        })
-        if (TrainingResult.data) {
-            toast('Training saved successfully')
-        }
-        if (TrainingResult.error) {
-            toast(TrainingResult.error + '')
-        }
+        }).then((data)=>toast(data.data.saveTraining.title+ ' Training saved successfully')).catch(err=>console.log(err))
+        .catch(err=>toast(err+ ''))
     }
     return (
-        <main >
-            <ToastContainer />
-            <div className="sticky-top">
-                <AcustomNavBar institutionName="Hospital Name" />
+        <div className="card px-3 py-3 mt-3">
+            <span className="fw-bolder fs-4 text-uppercase">Publish Training</span>
+            <div className="col-md-12 m-auto g-3 row">
+                <div className="col-md-3">
+                    <span className="fw-bold">Title</span>
+                    <input type="text" className="form-control border border-dark" value={trainingTitle} onChange={e => setTrainingTitle(e.target.value)} />
+                </div>
+                <div className="mb-3 col-md-3">
+                    <span className="fw-bold">Description</span>
+                    <div className="mb-3">
+                        <textarea className="form-control border border-dark" value={trainingDescription} onChange={e => setTrainingDescription(e.target.value)} rows={2}></textarea>
+                    </div>
+                </div>
+                <div className="mb-3 col-md-3">
+                    <span className="fw-bold">Trainers</span>
+                    <div className="mb-3">
+                        <textarea  className="form-control border border-dark" value={trainer} onChange={e => setTrainer(e.target.value)} rows={2}></textarea>
+                    </div>
+                </div>
+                <div className="mb-3 col-md-3">
+                    <span className="fw-bold">Deadline</span>
+                    <input type="datetime-local" value={trainingDeadline} onChange={e => setTrainingDeadline(e.target.value)} className="form-control border border-dark" />
+                </div>
             </div>
-            <section className="row col-12 m-auto  mt-5 overflow-auto">
-                <section className="col-md-3 overflow-auto" >
-                    <SideBar class="text-info fw-bolder" />
-                </section>
-                {trainingRecords()}
-            </section>
-        </main>
+            <button onClick={submitTrainingForm} className="text-uppercase btn w-25 d-block align-self-center text-white fw-bolder" style={{ backgroundColor: '#49579F' }}>
+                Register
+            </button>
+            <div>
+                <Link to={'/hospital/training'} className="fw-bolder nav-link" style={{ float: 'right', color: '#49579F' }}>View Trainings</Link>
+            </div>
+        </div>
     )
-
-    function trainingRecords() {
-        return <section className="col-md-9 overflow-auto">
-            <div className="card px-3 py-3 mt-3">
-                <span className="fw-bolder fs-4 text-uppercase">Publish Training</span>
-                <div className="col-md-12 m-auto g-3 row">
-                    <div className="form-floating mb-3 col-md-4">
-                        <input type="text" className="form-control" value={trainingTitle} onChange={e => setTrainingTitle(e.target.value)} />
-                        <label className="fw-bolder">Title</label>
-                    </div>
-                    <div className="form-floating mb-3 col-md-4">
-                        <div className="mb-3">
-                            <textarea className="form-control" value={trainingDescription} onChange={e => setTrainingDescription(e.target.value)} rows={2}></textarea>
-                        </div>
-                    </div>
-                    <div className="form-floating mb-3 col-md-4">
-                        <input type="datetime-local" value={trainingDeadline} onChange={e => setTrainingDeadline(e.target.value)} className="form-control" />
-                        <label className="fw-bolder">Deadline</label>
-                    </div>
-                </div>
-                <button onClick={submitTrainingForm} className="text-uppercase btn w-25 d-block align-self-center text-white fw-bolder" style={{ backgroundColor: '#49579F' }}>
-                    Register
-                </button>
-                <div>
-                    <span className="" style={{ float: 'right', color: '#49579F' }}>View Trainings</span>
-                </div>
-            </div>
-            {/*  */}
-            <div className="card px-3 py-3 mt-3">
-                <span className="fw-bolder fs-4 text-uppercase">Publish Jobs</span>
-                <div className="col-md-12 m-auto g-3 row">
-                    <div className="form-floating mb-3 col-md-4">
-                        <input type="text" className="form-control" />
-                        <label className="fw-bolder">Title</label>
-                    </div>
-                    <div className="form-floating mb-3 col-md-4">
-                        <div className="mb-3">
-                            <textarea className="form-control" name="" placeholder="Discription ....." id="" rows={2}></textarea>
-                        </div>
-                    </div>
-                    <div className="form-floating mb-3 col-md-4">
-                        <input type="date" className="form-control" />
-                        <label className="fw-bolder">Deadline</label>
-                    </div>
-                </div>
-                <button className="text-uppercase btn w-25 d-block align-self-center text-white fw-bolder" style={{ backgroundColor: '#49579F' }}>
-
-                </button>
-                <div>
-                    <span className="" style={{ float: 'right', color: '#49579F' }}>View Trainings</span>
-                </div>
-            </div>
-            {/*  */}
-            <div className="card px-3 py-3 mt-3">
-                <span className="fw-bolder fs-4 text-uppercase">Add Certificate</span>
-                <div className="col-md-12 m-auto g-3 row">
-                    <div className="form-floating mb-3 col-md-4">
-                        <input type="text" className="form-control" />
-                        <label className="fw-bolder">Title</label>
-                    </div>
-                    <div className="form-floating mb-3 col-md-4">
-                        <div className="mb-3">
-                            <textarea className="form-control" name="" placeholder="Discription ....." id="" rows={2}></textarea>
-                        </div>
-                    </div>
-                    <div className="form-floating mb-3 col-md-4">
-                        <input type="date" className="form-control" />
-                        <label className="fw-bolder">Deadline</label>
-                    </div>
-                </div>
-                <button className="text-uppercase btn w-25 d-block align-self-center text-white fw-bolder" style={{ backgroundColor: '#49579F' }}>Register</button>
-                <div>
-                    <span className="" style={{ float: 'right', color: '#49579F' }}>View Trainings</span>
-                </div>
-            </div>
-        </section>;
-    }
 }
-export default HospitalHome;
+function Certificate() {
+    return<div className="card px-3 py-3 mt-3">
+    <span className="fw-bolder fs-4 text-uppercase">Publish Certificate</span>
+    <div className="col-md-12 m-auto g-3 row">
+        <div className="col-md-3">
+            <span className="fw-bold">Title</span>
+            <input type="text" className="form-control border border-dark" />
+        </div>
+        <div className="mb-3 col-md-3">
+            <span className="fw-bold">Deescription</span>
+            <div className="mb-3">
+                <textarea className="form-control border border-dark"  rows={2}></textarea>
+            </div>
+        </div>
+        <div className="mb-3 col-md-3">
+            <span className="fw-bold">Deadline</span>
+            <input type="datetime-local" className="form-control border border-dark" />
+        </div>
+    </div>
+    <button  className="text-uppercase btn w-25 d-block align-self-center text-white fw-bolder" style={{ backgroundColor: '#49579F' }}>
+        Register
+    </button>
+    <div>
+        <Link to={'/hospital/certificate'} className="fw-bolder nav-link" style={{ float: 'right', color: '#49579F' }}>View Certificate</Link>
+    </div>
+</div>;
+}
+function Job() {
+    return <div className="card px-3 py-3 mt-3">
+    <span className="fw-bolder fs-4 text-uppercase">Publish Job</span>
+    <div className="col-md-12 m-auto g-3 row">
+        <div className="col-md-3">
+            <span className="fw-bold">Title</span>
+            <input type="text" className="form-control border border-dark" />
+        </div>
+        <div className="mb-3 col-md-3">
+            <span className="fw-bold">Description</span>
+            <div className="mb-3">
+                <textarea className="form-control border border-dark"  rows={2}></textarea>
+            </div>
+        </div>
+        <div className="mb-3 col-md-3">
+            <span className="fw-bold">Deadline</span>
+            <input type="datetime-local" className="form-control border border-dark" />
+        </div>
+    </div>
+    <button  className="text-uppercase btn w-25 d-block align-self-center text-white fw-bolder" style={{ backgroundColor: '#49579F' }}>
+        Register
+    </button>
+    <div>
+        <Link to={'/hospital/certificate'} className="fw-bolder nav-link" style={{ float: 'right', color: '#49579F' }}>View Jobs</Link>
+    </div>
+</div>;
+}
