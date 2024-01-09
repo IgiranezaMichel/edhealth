4628f554eeef410d8ff8734e606a460e9ddec2a3
IgiranezaMichel <igiramichael@gmail.com>
2024-01-09 19:46:26 +0200

    Remove space

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+ 
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 const client = new ApolloClient({
   uri: 'http://localhost:8080/graphql',
