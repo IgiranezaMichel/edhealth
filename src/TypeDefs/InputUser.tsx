2d1b2862ce9659d70646bec56081b45b6ef9b3b2
IgiranezaMichel <igiramichael@gmail.com>
2024-01-09 19:59:59 +0200

    Add InputUser type

---
+++ src/TypeDefs/InputUser.tsx
@@ -0,0 +1,11 @@
+type inputUser = {
+    id?:number,
+    name: string,
+    gender: string,
+    profilePicture: string,
+    phoneNumber: string
+    email: string,
+    dob: string,
+    role: string,
+    password?: string
+}
