518d07ea57508cd90d9224055a0953a6415d5f29
IgiranezaMichel <igiramichael@gmail.com>
2024-01-15 10:17:48 +0200

    add variables to be used

--- src/Pages/School/SchoolHome.tsx
+++ src/Pages/School/SchoolHome.tsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom"
 import { SchoolNavigation } from "../../Component/School/SchoolNavigation"
-
+import{useState,useEffect} from 'react'
 export const SchoolHome = () => {
+    const [firstName,setFirstName]=useState('');
+    const [lastName,setLastName]=useState('');
+    const [nationalId,setNationalId]=useState('');
+
     return (
         <SchoolNavigation>
             <div className="table-responsive-md rounded-5 bg-light mb-5 border border-dark p-3">
                 <table className="table table-light border-0">
                     <thead>
                         <tr>
-                            <th scope="col" className="p-3" colSpan={5}>Student Registered</th>
+                            <th scope="col" className="p-3" colSpan={5}>Register Student</th>
                             <th scope="col">
                                 <div className="float-end p-3">
                                     <input type="text" placeholder="search .." />
@@ -27,18 +31,47 @@ export const SchoolHome = () => {
                             <th>Action</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <td scope="row">Item</td>
-                            <td>Item</td>
-                            <td>Item</td>
-                            <td>Item</td>
-                            <td scope="row">Item</td>
-                            <td>Item</td>
-                        </tr>
-                    </tbody>
+       
                 </table>
             </div>
+            <section className="rounded-5 bg-light row col-12 m-auto">
+              <section className="row g-3 needs-validation">
+                <div className="col-md-4">
+                  <label className="form-label">First name</label>
+                  <input type="text" className="form-control" onChange={(e)=>setFirstName(e.target.value)} value={firstName} />
+                </div>
+                <div className="col-md-4">
+                  <label className="form-label">Last name</label>
+                  <input type="text" className="form-control" onChange={(e)=>setLastName(e.target.value)} value={lastName} />
+                </div>
+                <div className="col-md-4">
+                  <label   className="form-label">National Id</label>
+                  <div className="input-group">
+                    <span className="bi bi-person"></span>
+                    <input type="text" className="form-control" onChange={(e)=>setNationalId(e.target.value)} value={nationalId}/>
+                  </div>
+                </div>
+                 
+                <div className="col-md-4">
+                  <label className="form-label">Email</label>
+                  <input type="text" className="form-control" onChange={(e)=>setFirstName(e.target.value)} value={firstName} />
+                </div>
+                <div className="col-md-4">
+                  <label className="form-label">Last name</label>
+                  <input type="text" className="form-control" onChange={(e)=>setLastName(e.target.value)} value={lastName} />
+                </div>
+                <div className="col-md-4">
+                  <label   className="form-label">National Id</label>
+                  <div className="input-group">
+                    <span className="bi bi-person"></span>
+                    <input type="text" className="form-control" onChange={(e)=>setNationalId(e.target.value)} value={nationalId}/>
+                  </div>
+                </div>
+                <div className="col-12">
+                  <button className="btn btn-primary" type="submit">Submit form</button>
+                </div>
+              </section>
+            </section>
             {/*  */}
             <div className="card border border-black bg-light">
                 <div className="card-body">
