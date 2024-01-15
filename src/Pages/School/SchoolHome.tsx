1b8719ef69e80a7fa83d0a11067988fbec883993
IgiranezaMichel <igiramichael@gmail.com>
2024-01-15 17:31:50 +0200

    Arrage SchoolHome Components

--- src/Pages/School/SchoolHome.tsx
+++ src/Pages/School/SchoolHome.tsx
@@ -1,112 +1,98 @@
-import { Link } from "react-router-dom"
 import { SchoolNavigation } from "../../Component/School/SchoolNavigation"
-import{useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
+import { RegisterStudent } from "./RegisterStudent";
+import { useQuery } from "@apollo/client";
+import { SEARCH_STUDENT } from "../../GraphQl/Queries";
 export const SchoolHome = () => {
-    const [firstName,setFirstName]=useState('');
-    const [lastName,setLastName]=useState('');
-    const [nationalId,setNationalId]=useState('');
+  const [toDay, setToDay] = useState(new Date());
+  const [search, setSearch] = useState('');
+  const [schoolId, setSchoolId] = useState(2);
+  const [studentList, addStudentList] = useState([]);
+  const { data, loading, error } = useQuery(SEARCH_STUDENT, {
+    variables: { schoolId: schoolId, search: search }
+  });
+  useEffect(() => {
+    const searchStudent = async () => {
+      if (data) {
+        addStudentList(data.searchStudent);
+      }
+    }
+    if (search.length != 0) {
+      searchStudent();
+    }
+    const interval = setInterval(() => {
+      setToDay(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
 
-    return (
-        <SchoolNavigation>
-            <div className="table-responsive-md rounded-5 bg-light mb-5 border border-dark p-3">
-                <table className="table table-light border-0">
-                    <thead>
+  }, [toDay]);
+  const days = ['Saturday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Sunday'];
+  return (
+    <SchoolNavigation>
+      <div className="mb-5 fw-bold">
+        <span className="float-end d-block">
+          {toDay.getHours() < 12 ? <span>Good Morning <b>,</b></span> : toDay.getHours() == 12 ? <span>It's Lunch time, Enjoy the meal </span> : toDay.getHours() < 16 ?
+          <span>Good Afternoon <b>,</b></span> : <span>Good Evening  <b>,</b></span>}{days[toDay.getDay()]} {toDay.getHours() + ':' + toDay.getMinutes() + ':' + toDay.getSeconds()}
+        </span>
+      </div>
+      <section className="rounded-5 bg-body-tertiary border p-2 row col-12 m-auto d-flex align-content-center justify-content-center">
+        <section className="">
+          <div>
+            <input onChange={(e) => setSearch(e.target.value)} type="search" placeholder="Search ..." className="float-end form-control w-25 border border-primary-subtle mt-2" />
+          </div>
+          {search.length == 0 ? <RegisterStudent /> :
+            <main>
+              {loading && <div><div className="spinner-grow text-primary" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div></div>}
+              {
+                (studentList.length != 0) ? <main>
+                  <div
+                    className="table-responsive"
+                  >
+                    <table
+                      className="table table-primary mt-4"
+                    >
+                      <thead>
                         <tr>
-                            <th scope="col" className="p-3" colSpan={5}>Register Student</th>
-                            <th scope="col">
-                                <div className="float-end p-3">
-                                    <input type="text" placeholder="search .." />
-                                    <button>
-                                        <i className="bi bi-search" />
-                                    </button>
-                                </div>
-                            </th>
+                          <th scope="col">#</th>
+                          <th scope="col">Name</th>
+                          <th scope="col">Contact Detail</th>
+                          <th>Gender</th>
+                          <th>Date of Birth</th>
                         </tr>
-                        <tr>
-                            <th scope="col">National Id</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Email</th>
-                            <th>Gender</th>
-                            <th>Date of Birth</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-       
-                </table>
-            </div>
-            <section className="rounded-5 bg-light row col-12 m-auto">
-              <section className="row g-3 needs-validation">
-                <div className="col-md-4">
-                  <label className="form-label">First name</label>
-                  <input type="text" className="form-control" onChange={(e)=>setFirstName(e.target.value)} value={firstName} />
-                </div>
-                <div className="col-md-4">
-                  <label className="form-label">Last name</label>
-                  <input type="text" className="form-control" onChange={(e)=>setLastName(e.target.value)} value={lastName} />
-                </div>
-                <div className="col-md-4">
-                  <label   className="form-label">National Id</label>
-                  <div className="input-group">
-                    <span className="bi bi-person"></span>
-                    <input type="text" className="form-control" onChange={(e)=>setNationalId(e.target.value)} value={nationalId}/>
+                      </thead>
+                      <tbody>
+                        {
+                          studentList.map((data:any, index) => {
+                            return (
+                              <tr className="">
+                                <td scope="row">{index+1}</td>
+                                <td>{data.name}</td>
+                                <td>
+                                  <div><i className="bi bi-telephone"></i> {data.phoneNumber}</div>
+                                  <div><i className="bi bi-envelope"></i> {data.email}</div>
+                                </td>
+                                <td>{data.gender}</td>
+                                <td>
+                                <div><i className="bi bi-calendar"></i> {String(data.dob).split('T')[0]}</div>
+                                </td>
+                               
+                              </tr>
+                            )
+                          })
+                        }
+
+                      </tbody>
+                    </table>
                   </div>
-                </div>
-                 
-                <div className="col-md-4">
-                  <label className="form-label">Email</label>
-                  <input type="text" className="form-control" onChange={(e)=>setFirstName(e.target.value)} value={firstName} />
-                </div>
-                <div className="col-md-4">
-                  <label className="form-label">Last name</label>
-                  <input type="text" className="form-control" onChange={(e)=>setLastName(e.target.value)} value={lastName} />
-                </div>
-                <div className="col-md-4">
-                  <label   className="form-label">National Id</label>
-                  <div className="input-group">
-                    <span className="bi bi-person"></span>
-                    <input type="text" className="form-control" onChange={(e)=>setNationalId(e.target.value)} value={nationalId}/>
-                  </div>
-                </div>
-                <div className="col-12">
-                  <button className="btn btn-primary" type="submit">Submit form</button>
-                </div>
-              </section>
-            </section>
-            {/*  */}
-            <div className="card border border-black bg-light">
-                <div className="card-body">
-                    <h5 className="card-title fw-bold">View Academic Records</h5>
-                    <section className="row col-sm-10 m-auto">
-                        <div className="float-end p-3">
-                            <input className="rounded border-secondary float-end" type="text" placeholder="search .." />
-                        </div>
-                        <section className="col-sm-6">
-                            <div className="form-floating mb-3">
-                                <input type="email" className="form-control" />
-                                <label>Anual Report Marks</label>
-                            </div>
-                            <div className="form-floating mb-3">
-                                <input type="email" className="form-control" />
-                                <label>User Marks</label>
-                            </div>
-                        </section>
-                        <section className="col-sm-6">
-                            <div className="form-floating mb-3">
-                                <input type="email" className="form-control" />
-                                <label>Anual Description Marks</label>
-                            </div>
-                            <div className="form-floating mb-3">
-                                <input type="email" className="form-control" />
-                                <label>School Id</label>
-                            </div>
-                        </section>
-                        <section className="text-center">
-                            <button className="fw-bolder fs-5 text-light border-0 ro" style={{ backgroundColor: '#93499F' }}>REGISTER</button>
-                            <Link to={'/'} className="fw-bolder mx-3" style={{ color: '#93499F', textDecoration: 'none' }}>View Registered</Link>
-                        </section>
-                    </section>
-                </div>
-            </div>
-        </SchoolNavigation>
-    )
+
+                </main> : <div className="mt-5 p-3 text-center">Data not found</div>
+              }
+            </main>}
+        </section>
+      </section>
+
+    </SchoolNavigation>
+  )
 }
